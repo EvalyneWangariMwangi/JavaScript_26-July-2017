a1e2bed0f61fcd3332022b6e4264b31437068fda
Eva Mwangi <wmwangi@cytonn.com>
2017-07-26 08:40:42 +0000

    stating Js

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>JavaScript Objects, Functions, Closures and Prototypes</title>
+	<link rel="stylesheet" type="text/css" href="node_modules/foundation-sites/dist/css/foundation.min.css">
+  	<link rel="stylesheet" type="text/css" href="node_modules/foundation-sites/dist/css/foundation-float.css">
+   	<link rel="stylesheet" type="text/css" href="node_modules/foundation-sites/dist/css/foundation-prototype.css">
+ 	<link rel="stylesheet" type="text/css" href="node_modules/foundation-sites/dist/css/foundation-rtl.css"> 
+  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  	<script src="js/custom.js"></script>
+</head>
+<body>
+
+<form>
+	<input type="number" name="" placeholder="enter number">
+	<input type="number" name="" placeholder="enter another number">
+	<input type="button" name="" value="Add">
+	<input type="button" name="" value="Subtract">
+	<input type="button" name="" value="Multiply">
+	<input type="button" name="" value="Divide">
+</form>
+
+<!-- scripts for foundation -->
+<script type="text/javascript" src="node_modules/foundation-sites/dist/js/foundation.min.js"></script>
+<script>
+$(document).foundation();
+</script>
+<!-- end scripts for foundation -->
+</body>
+</html>
